2550e68d4b1a9992a3cfcc55a42a108a731a5997
CesarAugusto87 <cesara87@gmail.com>
2018-03-07 09:57:52 -0600

    Initial Commit

---
+++ randomizeText/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  randomizeText
+//
+//  Created by Cesar Augusto Sanchez Coraspe on 7/03/18.
+//  Copyright © 2018 Cesar Augusto Sanchez Coraspe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
